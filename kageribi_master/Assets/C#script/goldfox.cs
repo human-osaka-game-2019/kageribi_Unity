6073f027fbb0f4bf8dcf86c8bcc1844a20bdf066
yuma yasuda <yuma.handball7@gmail.com>
2019-11-12 10:49:34 +0900

    アニメーションの実装、属性入れ替えの実装

--- kageribi_master/Assets/C#script/goldfox.cs
+++ kageribi_master/Assets/C#script/goldfox.cs
@@ -8,15 +8,21 @@ public class goldfox : MonoBehaviour
 
     public RuntimeAnimatorController   Fire;
     public RuntimeAnimatorController  Water;
-   public GameObject Gin;
+    public GameObject Gin;
 
     private Animator animator;
+
+    Rigidbody2D rigid2D;
+    public float jumpForce = 390.0f;
+    int jumpCounts = 0;
 
 
     // Start is called before the first frame update
     void Start() //最初の一回のみ呼び出される
     {
       animator =  GetComponent<Animator>();
+      rigid2D = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
@@ -24,25 +30,45 @@ public class goldfox : MonoBehaviour
     {
         bool right = Input.GetKey(KeyCode.RightArrow);
         bool left = Input.GetKey(KeyCode.LeftArrow);
+        bool up = Input.GetKeyDown(KeyCode.UpArrow);
         //Animator animator = GetComponent<Animator>();
         //float GetAxis ("Horizintal") ←と→　を同時に取得できる。
-        if (right==true)
+
+        if (right == true)
         {
             transform.Translate(speed, 0.0f, 0.0f);//座標の更新　rigidbody .Addforce 
-            animator.SetInteger("Speed", 1);
+            animator.SetInteger("Right", 1);
         }
-        else if(left==true)
+        else if (left == true)
         {
             transform.Translate(-speed, 0.0f, 0.0f);
-            animator.SetInteger("Speed", -1);
+            animator.SetInteger("Left", 1);
         }
-        //else
-        //{
-        //animator.SetInteger("Speed", 0);
-        //{
+        else
+        {           
+            animator.SetInteger("Right", 0);
+            animator.SetInteger("Left", 0);
+        }
+        
+        if(up==true)
+        {
+            jumpCounts=jumpCounts+1;
+            if(jumpCounts==1)
+            {
+                animator.SetBool("Jump", true);
+                rigid2D.AddForce(transform.up * jumpForce);
 
-        //以下テストコード
-        if(Input.GetKeyDown(KeyCode.A))
+            }
+            if (jumpCounts==2)
+            {
+                animator.SetBool("Jump", true);
+                rigid2D.AddForce(transform.up * jumpForce);
+            }
+        }
+
+
+                //以下テストコード
+                if (Input.GetKeyDown(KeyCode.A))
         {
             animator.runtimeAnimatorController = Fire;
         }
@@ -58,6 +84,11 @@ public class goldfox : MonoBehaviour
             Destroy(this.gameObject);
             Debug.Log("kieta");
         }
+    }
+
+    void FinishJump()
+    {
+        animator.SetBool("Jump", false);
     }
 
 }
