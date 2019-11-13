8832068521903b891ec448f06e82e8e664929b54
Kazuma_Okamoto <rikazuma.culb@gmail.com>
2019-11-13 14:06:39 +0900

    背景グラデーションのテスト #4
    1枚のフィルターを背景に重ね、アルファ値を変更することで昼から夜の移り変わりを表現

---
+++ kageribi_master/Assets/BackGround/BackGroundGradation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackGroundGradation : MonoBehaviour
+{
+    public GameObject Filter;
+
+    float alpha = 0.0f;
+    public float speed;
+    bool ismax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ismax = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Filter.GetComponent<Renderer> ().material.color = new Color(1.0f, 1.0f, 1.0f, alpha);
+        
+        if (ismax == false)
+        {
+            alpha += speed;
+            if (alpha > 1.0f)
+            {
+                ismax = true;
+            }
+        }
+        else
+        {
+            alpha -= speed;
+            if (alpha < 0.0f)
+            {
+                ismax = false;
+            }
+        }
+    }
+}
