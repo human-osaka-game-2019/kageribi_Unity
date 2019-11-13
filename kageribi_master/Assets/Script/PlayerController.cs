70dc0007a442040d2ef74c8e093b4e27858e24b0
YeonSungMin <meisensei0312@hotmail.com>
2019-11-13 14:33:46 +0900

    Yeon / #8  Add Player(Gold) Move X position - Test Script

---
+++ kageribi_master/Assets/Script/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        if (Input.GetAxisRaw("Horizontal") > 0.19)
+        {
+            transform.Translate(0.5f, 0, 0);
+        }
+        
+        if (Input.GetAxisRaw("Horizontal") < -0.19)
+        {
+            transform.Translate(-0.5f, 0, 0);
+        }
+        
+        if (Input.GetAxisRaw("Vertical") > 0.19)
+        {
+
+        }
+
+        if (Input.GetAxisRaw("Vertical") < -0.19)
+        {
+
+        }
+    }
+}
