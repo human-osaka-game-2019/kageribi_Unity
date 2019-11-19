294114b60fa6020a577ec69382cad25f8298e5ca
Kazuma_Okamoto <rikazuma.culb@gmail.com>
2019-11-19 16:02:16 +0900

    太陽と月を回転させる #2
    地上の中心を基準に太陽と月を円運動させる

---
+++ kageribi_master/Assets/DayCycle/Rotation.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    [System.Serializable]
+    public class RotationObject
+    {
+        public GameObject RotationTexture;
+        public Vector3 Position;
+        float texture_width;
+
+        public void SetInitialPosition()
+        {
+            RotationTexture.gameObject.transform.position = new Vector3(Position.x, Position.y, Position.z);
+        }
+
+        public void ObtainWidth()
+        {
+            texture_width = RotationTexture.GetComponent<SpriteRenderer>().bounds.size.x;
+        }
+
+        public void RotateTexture(float speed)
+        {
+            RotationTexture.transform.RotateAround(new Vector3(0.0f, -5.4f, 0.0f), new Vector3(0.0f, 0.0f, 1.0f), -speed * Time.deltaTime);
+
+            RotationTexture.transform.Rotate(new Vector3(0.0f, 0.0f, speed * Time.deltaTime));
+        }
+    }
+
+    public float speed;
+
+    public RotationObject Sun = new RotationObject();
+    public RotationObject Moon = new RotationObject();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sun.SetInitialPosition();
+        Moon.SetInitialPosition();
+
+        Sun.ObtainWidth();
+        Moon.ObtainWidth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Sun.RotateTexture(speed);
+        Moon.RotateTexture(speed);
+
+        /*if (Sun.transform.position.x - (sun_width / 2) >= 9.6)
+        {
+            Moon.gameObject.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+        }*/
+    }
+}
