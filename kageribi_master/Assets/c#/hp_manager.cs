9ff7e04d46ceb3f2384f39670e1d78ba570968ee
森澤嘉宣 <morisawa49@gmail.com>
2019-11-21 15:26:36 +0900

    HP関連のcsファイル作成 HP関連のプレハブ作成

---
+++ kageribi_master/Assets/c#/hp_manager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hp_manager : MonoBehaviour
+{
+    [SerializeField] private GameObject gold;
+    [SerializeField] private GameObject silver;
+    [SerializeField] private int hp = 20;
+    GameObject[] gold_hp;
+    GameObject[] silver_hp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gold_hp[0] = GameObject.Find("HP1");
+        gold_hp[1] = GameObject.Find("HP2");
+        gold_hp[2] = GameObject.Find("HP3");
+        gold_hp[3] = GameObject.Find("HP4");
+        gold_hp[4] = GameObject.Find("HP5");
+        silver_hp[0] = GameObject.Find("HP1");
+        silver_hp[1] = GameObject.Find("HP2");
+        silver_hp[2] = GameObject.Find("HP3");
+        silver_hp[3] = GameObject.Find("HP4");
+        silver_hp[4] = GameObject.Find("HP5");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
