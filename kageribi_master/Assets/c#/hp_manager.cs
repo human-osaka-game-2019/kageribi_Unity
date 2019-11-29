89f26a7f66733aef94fe0a37daa960a819ce9be0
森澤嘉宣 <morisawa49@gmail.com>
2019-11-30 00:27:06 +0900

    時間によるUIの切り替え ダメージ時のUIの非表示

--- kageribi_master/Assets/c#/hp_manager.cs
+++ kageribi_master/Assets/c#/hp_manager.cs
@@ -9,9 +9,9 @@ public class hp_manager : MonoBehaviour
     [SerializeField] private GameObject silver;
     private GameObject[] gold_hp = new GameObject[20];
     private GameObject[] silver_hp = new GameObject[20];
-    int hp;
+    public int hp;
     int count = 0;
-    int Pdm = 0;
+    int Acount = 0;
     public int damage;
     public float night_timeout;
     public float morning_timeout;
@@ -42,7 +42,7 @@ public class hp_manager : MonoBehaviour
             silver_hp[count] = silver_child.gameObject;
             count += 1;
         }
-
+        count = 0;
         for (int i = 0; i < silver_hp.Length; i++) 
         {
             silver_hp[i].SetActive(false);
@@ -52,43 +52,47 @@ public class hp_manager : MonoBehaviour
             // Update is called once per frame
     void Update()
     {
-        Pdm = hp - damage;
+        int Pdm = hp - damage;
         time += Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             for (hp = hp; hp > Pdm; hp--)
             {
-                gold_hp[count].SetActive(false);
-                silver_hp[count].SetActive(false);
-                count--;
+                gold_hp[Acount].SetActive(false);
+                silver_hp[Acount].SetActive(false);
+                Acount++;
             }
         }
-
-        if (time >= night_timeout)
+        if (time > morning_timeout)
         {
-
-            for (int i = 0; i < hp; i++)
-            {                
-                silver_hp[i].SetActive(true);
-            }
-            for(int i = 0; i < gold_hp.Length; i++)
-            { 
-                gold_hp[i].SetActive(false);
-            }
-            time = 0;
-
-        }
-        else if (time >= morning_timeout)
-        {
+            count = 19;
             for (int i = 0; i < hp; i++)
             {               
-                gold_hp[i].SetActive(true);
+                gold_hp[count].SetActive(true);
+                count--;
             }
             for (int i = 0; i < silver_hp.Length; i++)
             {
                 silver_hp[i].SetActive(false);
             }
+            time = 0;
         }
+        else if (time > night_timeout)
+        {
+            count = 19;
+            for (int i = 0; i < hp; i++)
+            {                
+                silver_hp[count].SetActive(true);
+                count--;
+            }
+            for(int i = 0; i < gold_hp.Length; i++)
+            { 
+                gold_hp[i].SetActive(false);
+            }
+
+
+        }
+        
 
     }
     public void juge()
