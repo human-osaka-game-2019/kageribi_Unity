943d1ea4ad5f8fd19e7b11d4740d59f0299c6379
Kazuma_Okamoto <rikazuma.culb@gmail.com>
2019-12-18 16:46:58 +0900

    リザルト画面の動きを追加

--- kageribi_master/Assets/Script/ResultControl.cs
+++ kageribi_master/Assets/Script/ResultControl.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class ResultControl : MonoBehaviour
 {
+    public GameObject UI_ReturnTitle;
+
+    public float UI_switch;
+
+    private float UI_seconds;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +20,23 @@ public class ResultControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        UI_seconds += 1.0f * Time.deltaTime;
+
+        // Uiの点滅
+        if (UI_seconds >= UI_switch)
+        {
+            if (UI_ReturnTitle.activeSelf == true)
+            {
+                UI_ReturnTitle.SetActive(false);
+            }
+            else
+            {
+                UI_ReturnTitle.SetActive(true);
+            }
+
+            UI_seconds = 0.0f;
+        }
+
         if (Input.GetButtonDown("XBOXA"))
         {
             SceneManager.LoadScene("Title");
