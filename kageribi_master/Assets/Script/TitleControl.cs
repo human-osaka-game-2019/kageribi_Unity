c98987ee56c6f3be8c0d3983dba8dcc6620c540c
Kazuma_Okamoto <rikazuma.culb@gmail.com>
2019-12-18 16:15:10 +0900

    タイトルの動きを実装

--- kageribi_master/Assets/Script/TitleControl.cs
+++ kageribi_master/Assets/Script/TitleControl.cs
@@ -5,6 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class TitleControl : MonoBehaviour
 {
+    public GameObject TitleNoon, TitleNight;
+    public GameObject UI_Start;
+
+    public float UI_switch;
+    public float Title_switch;
+
+    private float UI_seconds;
+    private float Title_seconds;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +23,39 @@ public class TitleControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        UI_seconds += 1.0f * Time.deltaTime;
+        Title_seconds += 1.0f * Time.deltaTime;
+
+        // Uiの点滅
+        if (UI_seconds >= UI_switch)
+        {
+            if (UI_Start.activeSelf == true)
+            {
+                UI_Start.SetActive(false);
+            }
+            else
+            {
+                UI_Start.SetActive(true);
+            }
+
+            UI_seconds = 0.0f;
+        }
+        
+        // タイトルの切り替え
+        if (Title_seconds >= Title_switch)
+        {
+            if (TitleNoon.activeSelf == true)
+            {
+                TitleNoon.SetActive(false);
+            }
+            else
+            {
+                TitleNoon.SetActive(true);
+            }
+
+            Title_seconds = 0.0f;
+        }
+
         if (Input.GetButtonDown("XBOXA"))
         {
             SceneManager.LoadScene("Beta Release Version");
