09903295d366af0a1ae9fb149504b0e2b855d370
YeonSungMin <meisensei0312@hotmail.com>
2019-12-19 13:49:09 +0900

    Add Monster Pattern Red Blue

---
+++ kageribi_master/Assets/Monster C#/BulletMoveDLeft.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMoveDLeft : MonoBehaviour
+{
+    public float moveSpeed;
+    float Counts = 0;
+    float MaxCount = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveControl();
+    }
+
+    void MoveControl()
+    {
+        transform.Translate(Vector2.left * moveSpeed);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Counts += Time.deltaTime;
+
+        if (col.gameObject.tag == "Player" || col.gameObject.tag == "floor")
+        {
+            //gameObject.SetActive(false);
+            Destroy(gameObject);
+            Debug.Log("destroyed");
+        }
+        else if(Counts >= MaxCount)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
