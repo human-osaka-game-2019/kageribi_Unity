4e251dc13b2507d0ea98c4dd949f498360967d53
森澤嘉宣 <morisawa49@gmail.com>
2020-01-17 16:32:36 +0900

    乱数によるボスの属性の変更を実装

---
+++ kageribi_master/Assets/c#/master/Scene/ボス/bossAI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossAI : MonoBehaviour
+{
+    public RuntimeAnimatorController[] Boss;
+    Animator anim;
+    float ChangeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float ChangeCount;
+        ChangeTime += Time.deltaTime;
+        if (ChangeTime >= 1)
+        {
+            ChangeCount = Random.value;
+            if (ChangeCount <= 0.3)
+            {
+                anim.runtimeAnimatorController = Boss[0];
+                ChangeTime = 0;
+            }
+            else if (ChangeCount >= 0.7)
+            {
+                anim.runtimeAnimatorController = Boss[2];
+                ChangeTime = 0;
+            }
+            else
+            {
+                anim.runtimeAnimatorController = Boss[1];
+                ChangeTime = 0;
+            }
+        }
+    }
+}
