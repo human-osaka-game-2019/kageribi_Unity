f09541edc031bd7ab5674f48a25f7681ccaa3f6d
Kazuma_Okamoto <rikazuma.culb@gmail.com>
2020-02-19 16:18:02 +0900

    画面の拡大処理の調整

--- kageribi_master/Assets/Script/CameraControl.cs
+++ kageribi_master/Assets/Script/CameraControl.cs
@@ -15,12 +15,14 @@ public class CameraControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (this.transform.position.x >= 160)
+        if (this.transform.position.x >= 150)
         {
+            this.GetComponent<camera>().enabled = false;
+
             if (Camera.orthographicSize < 10)
             {
                 Camera.orthographicSize += 0.1f;
-                this.transform.Translate(0f, 0.5f, 0f);
+                this.transform.Translate(0.25f, 0.1f, 0f);
             }
         }
     }
