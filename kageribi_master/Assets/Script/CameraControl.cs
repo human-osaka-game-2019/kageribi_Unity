5e66df1e1b015ed7a22aefe223750e7051e16a17
Kazuma_Okamoto <rikazuma.culb@gmail.com>
2020-02-19 15:30:21 +0900

    画面の拡大

---
+++ kageribi_master/Assets/Script/CameraControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Camera Camera;
+    public GameObject CameraObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Camera.transform.position.x >= 150)
+        {
+            CameraObj.GetComponent<Camera>.enabled = false;
+
+            if (Camera.orthographicSize < 10)
+            {
+                Camera.orthographicSize += 0.1f;
+                CameraObj.transform.Translate(0f, 0.5f, 0f);
+            }
+        }
+    }
+}
